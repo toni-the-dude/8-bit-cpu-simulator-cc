59a2b4af4bf1793b48c897ca627c00080694256d
Ștefan Emil - Antoniu <76394495+toni-the-dude@users.noreply.github.com>
2023-04-27 17:26:14 +0300

    Declare base methods

--- CPU.py
+++ CPU.py
@@ -9,27 +9,54 @@ class CPU:
     class ControlUnit:
 
         def __init__(self):
-            print(" Successfully created ControlUnit object.")
+            print("-Successfully created ControlUnit object.")
+
+        def fetch_instructions(self):
+            pass
 
     class Processor:
 
         def __init__(self):
-            print(" Successfully created Processor object.")
+            print("-Successfully created Processor object.")
             self.alu = self.ALU()
-            self.regA = self.Register()
-            self.regB = self.Register()
+            self.regA = self.Register() # Accumulator
+            self.regB = self.Register() # Program Counter
 
         class ALU:
 
             def __init__(self):
-                print("  Successfully created ALU object.")
+                print("--Successfully created ALU object.")
+
+            def load(self):
+                pass
+
+            def store(self):
+                pass
+
+            def add(self):
+                pass
+
+            def sub(self):
+                pass
+
+            def mul(self):
+                pass
+
+            def div(self):
+                pass
+
+            def move(self):
+                pass
+
+            def cmp(self):
+                pass
 
         class Register:
 
             def __init__(self):
-                print("  Successfully created Register object.")
+                print("--Successfully created Register object.")
 
     class Cache:
 
         def __init__(self):
-            print(" Successfully created Cache object.")
+            print("-Successfully created Cache object.")
